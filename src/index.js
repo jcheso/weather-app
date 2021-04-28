6defc6bba69bdf781e2156e3ef0ef365c7c8c2fb
Jarryd Cheso <jarryd.cheso@gmail.com>
2021-04-28 23:08:17 +0800

    Added webpage skeleton and implemented API calls for geolocation and weather data

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
-import './styles.css';
+import "./styles.css";
 // import Icon from './icon.png';
 
+const weatherController = (() => {
+  async function getGeoLocation(location) {
+    const geoObj = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=d9e5b08d8639ae39ae8a23305b524d6b`
+    );
+    const geoData = await geoObj.json();
+    return geoData[0];
+  }
 
-const getSearch = (event) => {
-  let search = event.srcElement[0].value;
-  event.preventDefault();
-  return search;
-};
+  async function getWeatherData(geoData) {
+    const weatherObj = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${geoData.lat}&lon=${geoData.lon}&exclude={part}&appid=d9e5b08d8639ae39ae8a23305b524d6b`
+    );
+    const weatherData = await weatherObj.json();
+    return weatherData;
+  }
 
-async function getGifs(search) {
-  const img = document.querySelector("img");
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=qrSDUx6Fmx5NVUziNlhnkHofxHzfBF4v&s=${search}`,
-    { mode: "cors" }
-  );
-  const gifData = await response.json();
-  img.src = gifData.data.images.original.url;
-}
+  async function callWeatherAPI() {
+    const location = displayController.getSearchForm(event);
+    const geoData = await getGeoLocation(location);
+    const weatherData = await getWeatherData(geoData);
+    displayController.updateSite(weatherData);
+  }
 
-const gifSearch = (event) => {
-  let search = getSearch(event);
-  getGifs(search);
-};
+  return { callWeatherAPI };
+})();
 
-const searchForm = document.getElementById("searchForm");
-searchForm.addEventListener("submit", gifSearch);
+const displayController = (() => {
+  const addEventListeners = () => {
+    const searchForm = document.getElementById("searchForm");
+    searchForm.addEventListener("submit", weatherController.callWeatherAPI);
+  };
 
+  const getSearchForm = (event) => {
+    let location = event.srcElement[0].value;
+    event.preventDefault();
+    return location;
+  };
 
-let apiKey = d9e5b08d8639ae39ae8a23305b524d6b
+  const updateSite = (weatherData) => {
+    console.table(weatherData);
+    // Create function to create weather element for each day and loop through array
+  };
+
+  addEventListeners();
+
+  return { getSearchForm, updateSite };
+})();
+
+export { weatherController, displayController };
